595feab740ea8cf3182f1c9aede616c9be3eea0b
Po-hung Sung <100291884+Po-Hung0804@users.noreply.github.com>
2026-09-20 22:08:52 -0500

    Add: invert-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-2.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        // use dfs
+        /*
+        if(!root) return nullptr;
+        swap(root->left,root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+        */
+        // use bfs 
+        /*
+        if(!root) return nullptr;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* cur=q.front();
+            q.pop();
+            swap(cur->left,cur->right);
+            if(cur->left) q.push(cur->left);
+            if(cur->right) q.push(cur->right);
+        }
+        return root;
+        */
+        // iterative dfs
+        if(!root) return nullptr;
+        vector<TreeNode*> stk;
+        stk.push_back(root);
+        while(!stk.empty()){
+            TreeNode* cur=stk.back();
+            stk.pop_back();
+            swap(cur->left,cur->right);
+            if(cur->left) stk.push_back(cur->left);
+            if(cur->right) stk.push_back(cur->right);
+        }
+        return root;
+    }
+};
